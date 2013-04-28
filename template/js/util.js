5bce8878cd317912799532fe43e0249c39f19c2a
programmerazrz <programmer.azrz@gmail.com>
2013-04-28 23:54:50 +0430

    first git experice

---
+++ template/js/util.js
@@ -0,0 +1,16 @@
+
+(function () {
+    //*******************template Loader*************************//
+    function templateLoader(Object, address) {
+        $(Object).load(address, function () {
+            console.log(Object.toString() + "loaded Succecfully");
+        });
+
+    }
+    templateLoader("#header", 'js/templates/headerTemplate.html');
+    templateLoader("#content", 'js/templates/contentTemplate.html');
+    templateLoader("#footer", 'js/templates/footerTemplate.html');
+    //*******************template Loader*************************//
+
+    //*******************plugin Intializer*************************//
+})();
