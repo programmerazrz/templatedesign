78ad0f00bf7a407acd721b8b54dd2a36e47c3eb3
programmer.azrz <programmer.azrz@gmail.com>
2013-06-14 18:29:02 +0430

    footer

--- template/js/util.js
+++ template/js/util.js
@@ -11,22 +11,35 @@ var texts = {
     ProductImport:"تامین کالا",
     ProductExport:"خروج کالا",
     ReportExport:"گزارش خروج",
-    PmSubmit:"ثبت PM"
-
+    PmSubmit:"ثبت PM",
+    Copyright:"هوش مصنوعی شریف"
 
 };
 (function () {
+    //*******************Project Text Initializer*************************//
+    //please dont intial any text inside the dom elements learn procedure and
+    //use it
+     textIntializer = function (domObject){
+        jQuery(domObject).each(function(){
+            var textInside = $(this).attr("data-prop-text");
+            $(this).text(texts[textInside]);
+        })
+    };
+    //*******************Project Text Initializer*************************//
+
+
     //*******************template Loader*************************//
-    function templateLoader(Object, address) {
+    templateLoader = function (Object, address) {
         $(Object).load(address, function () {
             console.log(Object.toString() + "loaded Succecfully");
         });
-
-    }
+    };
     templateLoader("#header", 'js/templates/headerTemplate.html');
     templateLoader("#content", 'js/templates/contentTemplate.html');
     templateLoader("#footer", 'js/templates/footerTemplate.html');
     //*******************template Loader*************************//
+
+
 
     //*******************plugin Intializer*************************//
 })();
